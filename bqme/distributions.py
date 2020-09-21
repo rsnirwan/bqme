dd339347008087fb4c22c70e530cab9895bdd872
RSNirwan <rajbir_nirwan@yahoo.com>
2020-09-21 17:00:13 +0200

    Distribution and Variable classes added

---
+++ bqme/distributions.py
@@ -0,0 +1,51 @@
+from bqme.variables import ContinuousVariable, PositivContinousVariable
+
+class Distribution:
+    """
+    Base class for all distribution
+    """
+    def __init__(self,
+            parameters_dict: 'dict used for printing only',
+            name: str):
+        self.name = name
+        self.parameters_dict = parameters_dict
+
+    def __str__(self):
+        params = {name:param.value for name, param in 
+                    self.parameters_dict.items()}
+        return self.__class__.__name__ + '(' +  \
+            ', '.join([f'{k}={v}' for k,v in params.items()]) + \
+            f', name="{self.name}"' + ')'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Normal(Distribution):
+    """
+    Container for Normal Distribution
+    """
+    def __init__(self,
+            mu: float,
+            sigma: float,
+            name: str):
+        self.mu = ContinuousVariable(mu, name='mu')
+        self.sigma = PositivContinousVariable(sigma, name='sigma')
+        self.name = name
+        parameters_dict = {'mu': self.mu, 'sigma': self.sigma}
+        super().__init__(parameters_dict, self.name)
+
+
+class Gamma(Distribution):
+    """
+    Container for Gamma Distribution
+    """
+    def __init(self, 
+            alpha: float,
+            beta: float,
+            name: str):
+        self.alpha = PositivContinousVariable(alpha, name='alpha')
+        self.beta = PositivContinousVariable(alpha, name='beta')
+        self.name = name
+        parameters_dict = {'alpha':self.alpha, 'beta':self.beta}
+        super().__init__(parameters_dict, self.name)
---
+++ bqme/variables.py
@@ -0,0 +1,37 @@
+class Variable:
+    """
+    Base class for different kind of variables
+    """
+    def __init__(self, value, name, lower, upper):
+        self.lower = lower
+        self.upper = upper
+        self.name = name
+        self.value = self.approve(value)
+
+    def approve(self, value):
+        if self.lower < value < self.upper:
+            return value
+        else:
+            raise ValueError(f'Input parameter "{self.name}" needs to be in range ({self.lower}, {self.upper}), currently set to {value}.')
+
+
+class ContinuousVariable(Variable):
+    """
+    container for continuous variables (-inf, inf)
+    """
+    def __init__(self, value, name):
+        lower = float('-inf')
+        upper = float('inf')
+        super().__init__(value, name, lower, upper)
+
+
+class PositivContinousVariable(Variable):
+    """
+    container for positive continous variables (0, inf)
+    """
+    def __init__(self, value, name):
+        lower = 0
+        upper = float('inf')
+        super().__init__(value, name, lower, upper)
+
+
